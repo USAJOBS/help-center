ea600cb7f46c010a6ebc6d0c8def2b8868a1d712
Matthew Dingee <matthew.dingee@opm.gov>
2016-04-14 16:31:25 -0400

    Forward progress on contact us topics. [Amends #2]

--- js/components/contact_us.js
+++ js/components/contact_us.js
@@ -4,6 +4,7 @@ $help.on('change', '[data-behavior]', function (event) {
   var $el = $(this),
     $object = $el.closest('[data-object="help.contact_us"]'),
     behavior = $el.attr('data-behavior'),
+    selected_id = $el.val(),
     $target = $object.find('#' + $el.attr('aria-controls'));
 
   event.preventDefault();
@@ -11,16 +12,20 @@ $help.on('change', '[data-behavior]', function (event) {
 
   // Each behavior attached to the element should be triggered
   $.each(behavior.split(' '), function (idx, action) {
-    $el.trigger(action, { el: $el, object: $object, target: $target });
+    $el.trigger(action, { el: $el, object: $object, selected_id: selected_id, target: $target });
   });
 });
 
 $help.on('help.open', function(event, opts) {
-  var $send_button = $('#btn-send');
+  var $send_button = $('#btn-send'),
+    $all_topics = opts.target.find('.usajobs-help-topic'),
+    $target_topics = opts.target.find('.' + opts.selected_id);
 
   event.preventDefault();
 
   opts.target.attr('aria-hidden', 'false');
+  $all_topics.attr('aria-hidden', 'true');
+  $target_topics.attr('aria-hidden', 'false');
 
   // Enabled the send button
   $send_button.removeAttr('disabled');
