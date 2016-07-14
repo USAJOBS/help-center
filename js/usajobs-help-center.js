81c9ad9c02e7fb4437e51c11a592051b7aacb476
Matthew Dingee <matthew.dingee@opm.gov>
2016-07-14 16:25:34 -0400

    Converting military spouses page. [Amends #84]

--- js/usajobs-help-center.js
+++ js/usajobs-help-center.js
@@ -1984,3 +1984,32 @@ $help.on('help.open', function(event, opts) {
     opts.object.find('#contactOptionalData').attr('aria-hidden', 'true');
   }
 });
+
+var $help_accordion = $('[data-object="help-accordion"]');
+
+$help_accordion.on('click', '[data-behavior]', function (event) {
+  var $el = $(this),
+    $object = $el.closest('[data-object="help-accordion"]'),
+    behavior = $el.attr('data-behavior'),
+    $target = $object.find('#' + $el.attr('aria-controls')),
+    state = $target.attr('aria-hidden');
+
+  $el.blur(); // Removes focus
+
+  // Each behavior attached to the element should be triggered
+  $.each(behavior.split(' '), function (idx, action) {
+    if (action.match(/^help/)) {
+      $el.trigger(action, { el: $el, object: $object, target: $target, state: state });
+    }
+  });
+});
+
+$help_accordion.on('help-accordion.toggle', function(event, opts) {
+  event.preventDefault();
+
+  opts.target.slideDown(function () {
+    $('html, body').animate({
+      scrollTop: opts.object.offset().top
+    });
+  });
+});
