e06bce05ed58e1f37dc88f4648cdbe09d2feb377
Matthew Dingee <matthew.dingee@opm.gov>
2018-05-09 17:12:33 -0400

    Revises the secondary navigation in Help Center. [Amends #306]

--- js/usajobs-help-center-base.js
+++ js/usajobs-help-center-base.js
@@ -3315,8 +3315,6 @@ $nav.on('click', '[data-behavior]', function (event) {
 });
 
 $nav.on('nav.toggle', function(event, opts) {
-  event.preventDefault();
-
   if (opts.state === 'is-closed') {
     opts.object.trigger('nav.open', opts);
   } else if (opts.state === 'is-open') {
@@ -3346,8 +3344,6 @@ $nav.on('nav.menu.toggle', function(event, opts) {
     $menu = $parent.find('[role="menu"]'),
     $sibling = $parent.siblings().find('[aria-expanded="true"]');
 
-  event.preventDefault();
-
   $nav.trigger('nav.menu.slide-close', { parent: $sibling.parent(), menu: $sibling });
 
   if (parent_state === 'is-closed') {
@@ -3374,8 +3370,6 @@ $nav.on('nav.menu.slide-close', function(event, opts) {
 $nav.on('nav.menu.search-toggle', function(event, opts) {
   var $parent = opts.el.parent(),
     $sibling_trigger;
-
-  event.preventDefault();
 
   if (opts.state === 'false') {
     if (opts.sibling !== undefined && opts.sibling.length > 0) {
