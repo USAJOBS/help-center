203f4b6e7fca67a64cb9fa3dfab96558f190c2a3
Matthew Dingee <matthew.dingee@opm.gov>
2019-02-06 12:34:45 -0500

    Removes spurious config file and removes crufty options from Gruntfile.js. [Amends #352]

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,29 @@
 /*jshint node:true, esversion: 6 */
 
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
   // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+  require("time-grunt")(grunt);
 
   // Load grunt tasks listed in package.json automatically
-  require('load-grunt-tasks')(grunt);
+  require("load-grunt-tasks")(grunt);
 
-  const sass = require('node-sass');
+  const sass = require("node-sass");
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     // shell commands for use in Grunt tasks
     shell: {
       jekyllBuild: {
-        command: 'bundle exec jekyll build'
+        command: "bundle exec jekyll build"
       },
       jekyllServe: {
         command: "bundle exec jekyll serve --baseurl ''"
       },
       spellCheck: {
-        command: "mdspell -r -n -a --en-us faq/**/*.md how-to/**/*.md working-in-government/**/*.md"
+        command:
+          "mdspell -r -n -a --en-us faq/**/*.md how-to/**/*.md working-in-government/**/*.md"
       },
       linkChecker: {
         command: "blc http://usajobs.github.io/Help/ -e -r"
@@ -34,30 +35,31 @@ module.exports = function(grunt) {
           implementation: sass,
           sourceMap: true,
           relativeAssets: false,
-          outputStyle: 'expanded',
-          sassDir: '_scss',
-          cssDir: 'css',
+          outputStyle: "expanded",
+          sassDir: "_scss",
+          cssDir: "css",
           includePaths: [
-            'node_modules/bourbon/app/assets/stylesheets/',
-            'node_modules/bourbon-neat/app/assets/stylesheets/',
-            'node_modules/uswds/src/stylesheets/',
-            'node_modules/uswds/src/stylesheets/lib/',
-            'node_modules/usajobs-design-system/node_modules/bourbon/app/assets/stylesheets/',
-            'node_modules/usajobs-design-system/node_modules/bourbon-neat/app/assets/stylesheets/',
-            'node_modules/usajobs-design-system/node_modules/uswds/src/stylesheets/',
-            'node_modules/usajobs-design-system/node_modules/uswds/src/stylesheets/lib/',
-            'node_modules/usajobs-design-system/_scss/',
-            '_scss/'
-          ],
+            "node_modules/bourbon/app/assets/stylesheets/",
+            "node_modules/bourbon-neat/app/assets/stylesheets/",
+            "node_modules/uswds/src/stylesheets/",
+            "node_modules/uswds/src/stylesheets/lib/",
+            "node_modules/usajobs-design-system/node_modules/bourbon/app/assets/stylesheets/",
+            "node_modules/usajobs-design-system/node_modules/bourbon-neat/app/assets/stylesheets/",
+            "node_modules/usajobs-design-system/node_modules/uswds/src/stylesheets/",
+            "node_modules/usajobs-design-system/node_modules/uswds/src/stylesheets/lib/",
+            "node_modules/usajobs-design-system/_scss/",
+            "_scss/"
+          ]
         },
         files: [
           {
-            src: 'node_modules/usajobs-design-system/css/usajobs-design-system-base--help-center.css',
-            dest: 'css/usajobs-design-system-base--help-center.css'
+            src:
+              "node_modules/usajobs-design-system/css/usajobs-design-system-base--help-center.css",
+            dest: "css/usajobs-design-system-base--help-center.css"
           },
           {
-            src: '_scss/help-center.scss',
-            dest: 'css/help-center.css'
+            src: "_scss/help-center.scss",
+            dest: "css/help-center.css"
           }
         ]
       }
@@ -70,58 +72,58 @@ module.exports = function(grunt) {
       project_css: {
         expand: true,
         flatten: true,
-        src: 'css/*.css',
-        dest: 'css/'
+        src: "css/*.css",
+        dest: "css/"
       },
       layouts_css: {
         expand: true,
         flatten: true,
-        src: 'css/layouts/*.css',
-        dest: 'css/layouts/'
+        src: "css/layouts/*.css",
+        dest: "css/layouts/"
       }
     },
     cssmin: {
       minify: {
         src: [
-          'css/usajobs-design-system-base--help-center.css',
-          'css/help-center.css'
+          "css/usajobs-design-system-base--help-center.css",
+          "css/help-center.css"
         ],
-        dest: 'css/usajobs-help-center.min.css'
+        dest: "css/usajobs-help-center.min.css"
       }
     },
     browserify: {
       dist: {
-        src: [ 'js/glossary.js'],
-        dest: 'js/vendor/glossary.js'
+        src: ["js/glossary.js"],
+        dest: "js/vendor/glossary.js"
       }
     },
     concat: {
       base: {
         src: [
-          'node_modules/usajobs-design-system/js/usajobs-design-system-base.js',
-          'node_modules/usajobs-design-system/js/components/footer.js',
-          'node_modules/usajobs-design-system/js/components/modal.js',
-          'node_modules/usajobs-design-system/js/components/alert.js',
-          'node_modules/usajobs-design-system/js/components/nav.js',
-          'node_modules/usajobs-design-system/js/components/nav-secondary.js'
+          "node_modules/usajobs-design-system/js/usajobs-design-system-base.js",
+          "node_modules/usajobs-design-system/js/components/footer.js",
+          "node_modules/usajobs-design-system/js/components/modal.js",
+          "node_modules/usajobs-design-system/js/components/alert.js",
+          "node_modules/usajobs-design-system/js/components/nav.js",
+          "node_modules/usajobs-design-system/js/components/nav-secondary.js"
         ],
-        dest: 'js/usajobs-help-center-base.js'
+        dest: "js/usajobs-help-center-base.js"
       },
       components: {
         src: [
-          'node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
-          'js/vendor/picturefill.min.js',
-          'js/vendor/glossary.js',
-          'js/components/*.js'
+          "node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js",
+          "js/vendor/picturefill.min.js",
+          "js/vendor/glossary.js",
+          "js/components/*.js"
         ],
-        dest: 'js/usajobs-help-center-components.js'
+        dest: "js/usajobs-help-center-components.js"
       },
       prod: {
         src: [
-          'js/usajobs-help-center-base.js',
-          'js/usajobs-help-center-components.js'
+          "js/usajobs-help-center-base.js",
+          "js/usajobs-help-center-components.js"
         ],
-        dest: 'js/usajobs-help-center.js'
+        dest: "js/usajobs-help-center.js"
       }
     },
     uglify: {
@@ -130,25 +132,22 @@ module.exports = function(grunt) {
       },
       prod: {
         files: {
-          'js/usajobs-help-center.min.js': ['js/usajobs-help-center.js']
+          "js/usajobs-help-center.min.js": ["js/usajobs-help-center.js"]
         }
       }
     },
     jshint: {
       jshintrc: true,
       all: [
-        'Gruntfile.js',
-        'js/glossary.js',
-        'js/components/*.js',
-        '!js/vendor/*.js',
-        '!js/usajobs-help-center.js',
-        '!js/usajobs-help-center-components.js'
+        "Gruntfile.js",
+        "js/glossary.js",
+        "js/components/*.js",
+        "!js/vendor/*.js",
+        "!js/usajobs-help-center.js",
+        "!js/usajobs-help-center-components.js"
       ],
-      components: [
-        'js/glossary.js',
-        'js/components/*.js'
-      ],
-      gruntfile: 'Gruntfile.js'
+      components: ["js/glossary.js", "js/components/*.js"],
+      gruntfile: "Gruntfile.js"
     },
     notify_hooks: {
       options: {
@@ -159,39 +158,35 @@ module.exports = function(grunt) {
     },
     watch: {
       gruntfile: {
-        files: 'Gruntfile.js',
-        tasks: ['jshint:gruntfile']
+        files: "Gruntfile.js",
+        tasks: ["jshint:gruntfile"]
       },
       sass: {
-        files: [
-          '_scss/**/*.scss'
-        ],
-        tasks: ['sass'],
+        files: ["_scss/**/*.scss"],
+        tasks: ["sass"],
         options: {
           livereload: true
         }
       },
       base: {
         files: [
-          'js/*.js',
-          'js/terms.json',
-          '!js/vendor/*.js',
-          '!js/usajobs-help-center.js',
-          '!js/usajobs-design-system-base.js',
-          '!js/usajobs-design-system-components.js',
-          '!js/usajobs-design-system-documentation.js'
+          "js/*.js",
+          "js/terms.json",
+          "!js/vendor/*.js",
+          "!js/usajobs-help-center.js",
+          "!js/usajobs-design-system-base.js",
+          "!js/usajobs-design-system-components.js",
+          "!js/usajobs-design-system-documentation.js"
         ],
-        tasks: ['jshint:all', 'concat:base'],
+        tasks: ["jshint:all", "concat:base"],
         options: {
           debounceDelay: 250,
           livereload: true
         }
       },
       components: {
-        files: [
-          'js/components/*.js'
-        ],
-        tasks: ['jshint:components', 'concat:components'],
+        files: ["js/components/*.js"],
+        tasks: ["jshint:components", "concat:components"],
         options: {
           debounceDelay: 250,
           livereload: true
@@ -200,11 +195,7 @@ module.exports = function(grunt) {
     },
     // run tasks in parallel
     concurrent: {
-      serve: [
-        'css',
-        'watch',
-        'shell:jekyllServe'
-      ],
+      serve: ["css", "watch", "shell:jekyllServe"],
       options: {
         logConcurrentOutput: true
       }
@@ -212,23 +203,32 @@ module.exports = function(grunt) {
     htmllint: {
       all: {
         options: {
-          errorlevels: ['error'],
+          errorlevels: ["error"],
           ignore: [
-            'Element “img” is missing required attribute “src”.',
-            'Attribute “aria-labeledby” not allowed on element “ul” at this point.',
-            'The “main” element must not appear as a descendant of the “section” element.'
+            "Element “img” is missing required attribute “src”.",
+            "Attribute “aria-labeledby” not allowed on element “ul” at this point.",
+            "The “main” element must not appear as a descendant of the “section” element."
           ]
         },
-        src: [ '_dist/**/*.html' ]
+        src: ["_dist/**/*.html"]
       }
     }
   });
 
-  grunt.registerTask('serve', ['concurrent:serve']);
-  grunt.registerTask('build', ['shell:jekyllBuild', 'css', 'js']);
-  grunt.registerTask('build-flat', ['shell:jekyllBuildFlat']);
-  grunt.registerTask('build-prod', ['shell:jekyllBuildProd']);
-  grunt.registerTask('css', ['sass', 'autoprefixer', 'cssmin']);
-  grunt.registerTask('js', ['jshint:all', 'concat:base', 'browserify', 'concat:components', 'concat:prod', 'uglify:prod']);
-  grunt.registerTask('test', ['shell:spellCheck', 'htmllint', 'shell:linkChecker']);
+  grunt.registerTask("serve", ["concurrent:serve"]);
+  grunt.registerTask("build", ["shell:jekyllBuild", "css", "js"]);
+  grunt.registerTask("css", ["sass", "autoprefixer", "cssmin"]);
+  grunt.registerTask("js", [
+    "jshint:all",
+    "concat:base",
+    "browserify",
+    "concat:components",
+    "concat:prod",
+    "uglify:prod"
+  ]);
+  grunt.registerTask("test", [
+    "shell:spellCheck",
+    "htmllint",
+    "shell:linkChecker"
+  ]);
 };
